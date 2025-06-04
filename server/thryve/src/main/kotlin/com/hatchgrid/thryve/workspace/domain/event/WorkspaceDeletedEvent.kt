9c61bb4325cb94f331ac1aedc34ee96208efdd72
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-06-04 09:34:32 +0200

    refactor(workspace): 🔧 rename workspaceId to id in WorkspaceDeletedEvent

--- server/thryve/src/main/kotlin/com/hatchgrid/thryve/workspace/domain/event/WorkspaceDeletedEvent.kt
+++ server/thryve/src/main/kotlin/com/hatchgrid/thryve/workspace/domain/event/WorkspaceDeletedEvent.kt
@@ -5,8 +5,8 @@ import com.hatchgrid.common.domain.bus.event.BaseDomainEvent
 /**
  * Event that represents a workspace being deleted.
  *
- * @property workspaceId The id of the workspace that was deleted.
+ * @property id The id of the workspace that was deleted.
  */
 data class WorkspaceDeletedEvent(
-    val workspaceId: String
+    val id: String
 ) : BaseDomainEvent()
