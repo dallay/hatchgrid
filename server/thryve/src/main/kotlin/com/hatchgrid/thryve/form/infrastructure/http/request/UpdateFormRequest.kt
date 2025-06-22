468c71c5b452c3901fa53bef295523a169cfa2b0
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-06-22 23:38:41 +0200

    feat(form): add validation for hex color codes and field size limits

--- server/thryve/src/main/kotlin/com/hatchgrid/thryve/form/infrastructure/http/request/UpdateFormRequest.kt
+++ server/thryve/src/main/kotlin/com/hatchgrid/thryve/form/infrastructure/http/request/UpdateFormRequest.kt
@@ -1,5 +1,6 @@
 package com.hatchgrid.thryve.form.infrastructure.http.request
 
+import com.hatchgrid.thryve.AppConstants.HEXADECIMAL_COLOR_PATTERN
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
@@ -23,29 +24,29 @@ data class UpdateFormRequest(
     @field:NotBlank(message = "Button color is required")
     @field:Size(max = 7, message = "Button color must be a valid hex code")
     @field:Pattern(
-        regexp = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
-        message = "Button color must be a valid hex code"
+        regexp = HEXADECIMAL_COLOR_PATTERN,
+        message = "Button color must be a valid hex code",
     )
     val buttonColor: String,
     @field:NotBlank(message = "Background color is required")
     @field:Size(max = 7, message = "Background color must be a valid hex code")
     @field:Pattern(
-        regexp = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
-        message = "Background color must be a valid hex code"
+        regexp = HEXADECIMAL_COLOR_PATTERN,
+        message = "Background color must be a valid hex code",
     )
     val backgroundColor: String,
     @field:NotBlank(message = "Text color is required")
     @field:Size(max = 7, message = "Text color must be a valid hex code")
     @field:Pattern(
-        regexp = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
-        message = "Text color must be a valid hex code"
+        regexp = HEXADECIMAL_COLOR_PATTERN,
+        message = "Text color must be a valid hex code",
     )
     val textColor: String,
     @field:NotBlank(message = "Button text color is required")
     @field:Size(max = 7, message = "Button text color must be a valid hex code")
     @field:Pattern(
-        regexp = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
-        message = "Button text color must be a valid hex code"
+        regexp = HEXADECIMAL_COLOR_PATTERN,
+        message = "Button text color must be a valid hex code",
     )
     val buttonTextColor: String
 )
