8f0467541b7fb29893aa82ed307ded67e1fcf968
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-08-02 23:19:11 +0200

    chore(playwright): 🛠 update base URL and adjust worker settings for CI

--- client/apps/web/playwright.config.ts
+++ client/apps/web/playwright.config.ts
@@ -11,14 +11,15 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	/* Allow parallel workers on CI for better performance. */
+	// You can set a specific number, e.g. 4, or leave undefined for Playwright's default
+	workers: process.env.CI ? undefined : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://localhost:5173",
+	   baseURL: "http://localhost:9876",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
